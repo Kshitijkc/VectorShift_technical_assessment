180b1406787ad9b84b3bfa9da0965e449c61eeb2
Kshitij Kumar <kshitijkumarchoudhary@gmail.com>
2025-01-12 13:21:49 +0530

    Completed Part 2: Loading HubSpot Items and bug fixes related to encoding and decoding saved state

--- backend/integrations/hubspot.py
+++ backend/integrations/hubspot.py
@@ -7,6 +7,8 @@ from fastapi.responses import HTMLResponse
 import base64
 import httpx
 import asyncio
+import requests
+from integrations.integration_item import IntegrationItem
 
 from redis_client import add_key_value_redis, get_value_redis, delete_key_redis
 
@@ -15,7 +17,7 @@ CLIENT_SECRET = 'XXX'
 encoded_client_id_secret = base64.b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode()).decode()
 
 REDIRECT_URI = 'http://localhost:8000/integrations/hubspot/oauth2callback'
-authorization_url = f'https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri=http://localhost:8000/integrations/hubspot/oauth2callback&scope=oauth'
+authorization_url = f'https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri=http://localhost:8000/integrations/hubspot/oauth2callback&scope=oauth%20crm.objects.contacts.read'
 
 async def authorize_hubspot(user_id, org_id):
     # TODO
@@ -24,7 +26,7 @@ async def authorize_hubspot(user_id, org_id):
         'user_id': user_id,
         'org_id': org_id
     }
-    encoded_state = json.dumps(state_data)
+    encoded_state = base64.urlsafe_b64encode(json.dumps(state_data).encode('utf-8')).decode('utf-8')
     await add_key_value_redis(f'hubspot_state:{org_id}:{user_id}', encoded_state, expire=600)
 
     return f'{authorization_url}&state={encoded_state}'
@@ -35,7 +37,7 @@ async def oauth2callback_hubspot(request: Request):
         raise HTTPException(status_code=400, detail=request.query_params.get('error'))
     code = request.query_params.get('code')
     encoded_state = request.query_params.get('state')
-    state_data = json.loads(encoded_state)
+    state_data = json.loads(base64.urlsafe_b64decode(encoded_state).decode('utf-8'))
 
     original_state = state_data.get('state')
     user_id = state_data.get('user_id')
@@ -43,7 +45,10 @@ async def oauth2callback_hubspot(request: Request):
 
     saved_state = await get_value_redis(f'hubspot_state:{org_id}:{user_id}')
 
-    if not saved_state or original_state != json.loads(saved_state).get('state'):
+    # Convert saved_state to a Python dictionary
+    saved_state = json.loads(base64.urlsafe_b64decode(saved_state).decode('utf-8'))
+
+    if not saved_state or original_state != saved_state.get('state'):
         raise HTTPException(status_code=400, detail='State does not match.')
 
     async with httpx.AsyncClient() as client:
@@ -88,10 +93,33 @@ async def get_hubspot_credentials(user_id, org_id):
 
     return credentials
 
-async def create_integration_item_metadata_object(response_json):
+def create_integration_item_metadata_object(response_json) -> IntegrationItem:
     # TODO
-    pass
+    integration_item_metadata = IntegrationItem(
+        id=response_json.get('vid', None),
+        name=f"{response_json.get('properties').get('firstname').get('value')} {response_json.get('properties').get('lastname').get('value')}"
+    )
 
-async def get_items_hubspot(credentials):
+    return integration_item_metadata
+
+async def get_items_hubspot(credentials) -> list[IntegrationItem]:
     # TODO
-    pass
+    """Aggregates all metadata relevant for a hubspot integration"""
+    credentials = json.loads(credentials)
+    response = requests.get(
+        'https://api.hubapi.com/contacts/v1/lists/all/contacts/all',
+        headers={
+            'Authorization': f'Bearer {credentials.get("access_token")}',
+            'Content-Type': 'application/json',
+        },
+    )
+    
+    list_of_integration_item_metadata = []
+    if response.status_code == 200:
+        contacts = response.json()['contacts']
+        for contact in contacts:
+            list_of_integration_item_metadata.append(
+                create_integration_item_metadata_object(contact)
+            )
+    
+    return list_of_integration_item_metadata
